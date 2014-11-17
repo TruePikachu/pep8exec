9c5cfce3808fc651a190a67c3bfc7a963b1949d9
Chris Dusto <cricket_lover@live.com>
2014-11-16 19:51:11 -0800

    CPU-based class, and a lot of the commands

---
+++ pep8cpu.cpp
@@ -0,0 +1,331 @@
+#include "pep8cpu.hpp"
+#include <cstdio>
+#include <ostream>
+#include <stdexcept>
+#include <stdint.h>
+using namespace std;
+
+Pep8CPU::Pep8CPU(Pep8Memory&memory) : memory(memory) {
+	PC=0;
+	SP=0;
+	IR.OP=0;
+	IR.PAR=0;
+}
+
+const Pep8Memory& Pep8CPU::getMemory() const {
+	return memory;
+}
+
+Pep8Memory& Pep8CPU::getMemory() {
+	return memory;
+}
+
+const Pep8Flags& Pep8CPU::getFlags() const {
+	return NZVC;
+}
+
+Pep8Flags& Pep8CPU::getFlags() {
+	return NZVC;
+}
+
+const Pep8Register& Pep8CPU::getA() const {
+	return A;
+}
+
+Pep8Register& Pep8CPU::getA() {
+	return A;
+}
+
+const Pep8Register& Pep8CPU::getX() const {
+	return X;
+}
+
+Pep8Register& Pep8CPU::getX() {
+	return X;
+}
+
+uint16_t Pep8CPU::getPC() const {
+	return PC;
+}
+
+Pep8CPU& Pep8CPU::setPC(uint16_t newPC) {
+	PC=newPC;
+	return *this;
+}
+
+uint16_t Pep8CPU::getSP() const {
+	return SP;
+}
+
+Pep8CPU& Pep8CPU::setSP(uint16_t newSP) {
+	SP=newSP;
+	return *this;
+}
+
+bool Pep8CPU::doInstruction() {
+	// Fetch
+	IR.OP=memory.getUB(PC);
+	// Read&Increment
+	PC++;
+	Instruction inst;
+	if(IR.OP<BR)
+		inst=(Instruction)(IR.OP&0xFF);
+	else if(IR.OP<NOPn)
+		inst=(Instruction)(IR.OP&0xFE);
+	else if(IR.OP<NOP)
+		inst=(Instruction)(IR.OP&0xFC);
+	else if(IR.OP<ADDr)
+		inst=(Instruction)(IR.OP&0xF8);
+	else
+		inst=(Instruction)(IR.OP&0xF0);
+	AddressMode addr;
+	switch(inst) {
+		case BR:
+		case BRLE:
+		case BRLT:
+		case BREQ:
+		case BRNE:
+		case BRGE:
+		case BRGT:
+		case BRV:
+		case BRC:
+		case CALL:
+			if(IR.OP&1)
+				addr=ADRx;
+			else
+				addr=ADRi;
+			break;
+		case NOP:
+		case DECI:
+		case DECO:
+		case STRO:
+		case CHARI:
+		case CHARO:
+		case ADDSP:
+		case SUBSP:
+		case ADDr:
+		case SUBr:
+		case ANDr:
+		case ORr:
+		case CPr:
+		case LDr:
+		case LDBYTEr:
+		case STr:
+		case STBYTEr:
+			addr=(AddressMode)(IR.OP&07);
+			break;
+		default:
+			addr=ADRnone;
+	}
+	if(addr!=ADRnone) {
+		IR.PAR=memory.getUW(PC);
+		PC+=2;
+	}
+	// Execute
+	Pep8Register* r = NULL;
+	if(BR<=inst && inst<=CALL)
+		if(IR.OP&1)
+			r=&X;
+		else
+			r=&A;
+	if(ADDr<=inst)
+		if(IR.OP&8)
+			r=&X;
+		else
+			r=&A;
+	uint16_t parameter;
+	switch(addr) {
+		case ADRnone:
+			break;
+		case ADRi:
+			parameter=IR.PAR;
+			break;
+		case ADRd:
+			parameter=memory.getUW(IR.PAR);
+			break;
+		case ADRn:
+			parameter=memory.getUW(memory.getUW(IR.PAR));
+			break;
+		case ADRs:
+			parameter=memory.getUW(SP+IR.PAR);
+			break;
+		case ADRsf:
+			parameter=memory.getUW(memory.getUW(SP+IR.PAR));
+			break;
+		case ADRx:
+			parameter=memory.getUW(X.getUW()+IR.PAR);
+			break;
+		case ADRsx:
+			parameter=memory.getUW(SP+X.getUW()+IR.PAR);
+			break;
+		case ADRsxf:
+			parameter=memory.getUW(memory.getUW(SP+IR.PAR)+X.getUW());
+			break;
+	}
+	switch(inst) {
+		case STOP:
+			break;
+		case RETTR:
+			NZVC.setBits(0,3,memory.getUW(SP));
+			A.setUW(memory.getUW(SP+1));
+			X.setUW(memory.getUW(SP+3));
+			PC=memory.getUW(SP+5);
+			SP=memory.getUW(SP+7);
+			break;
+		case MOVSPA:
+			A.setUW(SP);
+			break;
+		case MOVFLGA:
+			A.setBits(0,11,0);
+			A.setBits(12,15,NZVC.getBits(0,3));
+			break;
+		case BR:
+			PC = parameter;
+			break;
+		case BRLE:
+			if(NZVC.getN() || NZVC.getZ())
+				PC = parameter;
+			break;
+		case BRLT:
+			if(NZVC.getN())
+				PC = parameter;
+			break;
+		case BREQ:
+			if(NZVC.getZ())
+				PC = parameter;
+			break;
+		case BRNE:
+			if(!NZVC.getZ())
+				PC = parameter;
+			break;
+		case BRGE:
+			if(!NZVC.getN())
+				PC = parameter;
+			break;
+		case BRGT:
+			if(!NZVC.getN() && !NZVC.getZ())
+				PC = parameter;
+			break;
+		case BRV:
+			if(NZVC.getV())
+				PC = parameter;
+			break;
+		case BRC:
+			if(NZVC.getC())
+				PC = parameter;
+			break;
+		case CALL:
+			SP -= 2;
+			memory.setUW(SP,PC);
+			PC = parameter;
+			break;
+		case NOTr:
+			r->setUW(~r->getUW());
+			NZVC.setN(r->getBit(0));
+			NZVC.setZ(r->getUW()==0);
+			break;
+		case ASRr:
+			NZVC.setC(r->getBit(15));
+			r->setBits(1,15,r->getBits(0,14));
+			NZVC.setN(r->getBit(0));
+			NZVC.setZ(r->getUW()==0);
+			break;
+		case ROLr:
+			NZVC.setC(r->getBit(0));
+			r->setBits(0,14,r->getBits(1,15));
+			r->setBit(15,NZVC.getC());
+			break;
+		case RORr:
+			NZVC.setC(r->getBit(15));
+			r->setBits(1,15,r->getBits(0,14));
+			r->setBit(0,NZVC.getC());
+			break;
+		case RETn:
+			SP += parameter;
+			PC = memory.getUW(SP);
+			SP += 2;
+			break;
+		case ANDr:
+			r->setUW(r->getUW() & parameter);
+			NZVC.setN(r->getBit(15));
+			NZVC.setZ(r->getUW()==0);
+			break;
+		case ORr:
+			r->setUW(r->getUW() | parameter);
+			NZVC.setN(r->getBit(15));
+			NZVC.setZ(r->getUW()==0);
+			break;
+		case LDr:
+			r->setUW(parameter);
+			NZVC.setN(r->getBit(15));
+			NZVC.setZ(r->getUW()==0);
+			break;
+		case LDBYTEr:
+			r->setUB(parameter);
+			NZVC.setN(r->getBit(15));
+			NZVC.setZ(r->getUW()==0);
+			break;
+		case STr:
+			switch(addr) {
+				case ADRd:
+					memory.setUW(IR.PAR,r->getUW());
+					break;
+				case ADRn:
+					memory.setUW(memory.getUW(IR.PAR),r->getUW());
+					break;
+				case ADRs:
+					memory.setUW(SP+IR.PAR,r->getUW());
+					break;
+				case ADRsf:
+					memory.setUW(memory.getUW(SP+IR.PAR),r->getUW());
+					break;
+				case ADRx:
+					memory.setUW(X.getUW()+IR.PAR,r->getUW());
+					break;
+				case ADRsx:
+					memory.setUW(SP+X.getUW()+IR.PAR,r->getUW());
+					break;
+				case ADRsxf:
+					memory.setUW(memory.getUW(SP+IR.PAR)+X.getUW(),r->getUW());
+					break;
+			}
+			break;
+		case STBYTEr:
+			switch(addr) {
+				case ADRd:
+					memory.setUB(IR.PAR,r->getUB());
+					break;
+				case ADRn:
+					memory.setUB(memory.getUW(IR.PAR),r->getUB());
+					break;
+				case ADRs:
+					memory.setUB(SP+IR.PAR,r->getUB());
+					break;
+				case ADRsf:
+					memory.setUB(memory.getUW(SP+IR.PAR),r->getUB());
+					break;
+				case ADRx:
+					memory.setUB(X.getUW()+IR.PAR,r->getUB());
+					break;
+				case ADRsx:
+					memory.setUB(SP+X.getUW()+IR.PAR,r->getUB());
+					break;
+				case ADRsxf:
+					memory.setUB(memory.getUW(SP+IR.PAR)+X.getUW(),r->getUB());
+					break;
+			}
+			break;
+		default:
+			char errorBuf[64];
+			sprintf(errorBuf,"Command 0x%02X unimplemented",IR.OP);
+			throw logic_error(errorBuf);
+	}
+
+	// ReturnValue
+	return inst!=STOP;
+}
+
+bool Pep8CPU::doInstruction(uint16_t where) {
+	PC=where;
+	return doInstruction();
+}
---
+++ pep8cpu.hpp
@@ -0,0 +1,104 @@
+#ifndef __PEP8CPU_HPP
+#define __PEP8CPU_HPP
+class Pep8CPU;
+#include "pep8mem.hpp"
+#include <ostream>
+#include <stdint.h>
+
+class Pep8CPU {
+	friend std::ostream& operator<<(std::ostream&,const Pep8CPU&);
+	private:
+		typedef enum {
+			STOP	= 0x00,
+			RETTR	= 0x01,
+			MOVSPA	= 0x02,
+			MOVFLGA	= 0x03,
+
+			// Mask:  0xFE
+			BR	= 0x04,
+			BRLE	= 0x06,
+			BRLT	= 0x08,
+			BREQ	= 0x0A,
+			BRNE	= 0x0C,
+			BRGE	= 0x0E,
+			BRGT	= 0x10,
+			BRV	= 0x12,
+			BRC	= 0x14,
+			CALL	= 0x16,
+
+			NOTr	= 0x18,
+			NEGr	= 0x1A,
+			ASLr	= 0x1C,
+			ASRr	= 0x1E,
+			ROLr	= 0x20,
+			RORr	= 0x22,
+
+			// Mask:  0xFC
+			NOPn	= 0x24,
+			// Mask:  0xF8
+			NOP	= 0x28,
+
+			DECI	= 0x30,
+			DECO	= 0x38,
+			STRO	= 0x40,
+			CHARI	= 0x48,
+			CHARO	= 0x50,
+			
+			RETn	= 0x58,
+
+			ADDSP	= 0x60,
+			SUBSP	= 0x68,
+
+			// Mask:  0xF0
+			ADDr	= 0x70,
+			SUBr	= 0x80,
+			ANDr	= 0x90,
+			ORr	= 0xA0,
+			CPr	= 0xB0,
+
+			LDr	= 0xC0,
+			LDBYTEr	= 0xD0,
+			STr	= 0xE0,
+			STBYTEr	= 0xF0	} Instruction;
+		typedef enum {
+			ADRnone	= 0xFF,
+			ADRi	= 00,
+			ADRd	= 01,
+			ADRn	= 02,
+			ADRs	= 03,
+			ADRsf	= 04,
+			ADRx	= 05,
+			ADRsx	= 06,
+			ADRsxf	= 07	} AddressMode;
+
+
+
+		Pep8Memory&	memory;
+		Pep8Flags	NZVC;
+		Pep8Register	A;
+		Pep8Register	X;
+		uint16_t	PC;
+		uint16_t	SP;
+		struct {
+			uint8_t		OP;
+			uint16_t	PAR;
+			}	IR;
+	public:
+					 Pep8CPU	(Pep8Memory&);
+		const Pep8Memory&	getMemory	() const;
+		Pep8Memory&		getMemory	();
+		const Pep8Flags&	getFlags	() const;
+		Pep8Flags&		getFlags	();
+		const Pep8Register&	getA		() const;
+		Pep8Register&		getA		();
+		const Pep8Register&	getX		() const;
+		Pep8Register&		getX		();
+		uint16_t		getPC		() const;
+		Pep8CPU&		setPC		(uint16_t);
+		uint16_t		getSP		() const;
+		Pep8CPU&		setSP		(uint16_t);
+		bool			doInstruction	();	// Returns FALSE on STOP
+		bool			doInstruction	(uint16_t);	// Set PC first
+};
+
+#endif
