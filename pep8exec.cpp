0d3aae989306314346880ec54820e7834e08a614
Chris Dusto <cricket_lover@live.com>
2014-11-19 18:04:10 -0800

    Working menu (with exceptions of dumping and tracing)

--- pep8exec.cpp
+++ pep8exec.cpp
@@ -1,6 +1,7 @@
 #include "pep8cpu.hpp"
 #include "pep8mem.hpp"
 #include <cstdlib>
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -96,35 +97,89 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
+void doLoad(const char*);
+void doExecute();
+void doDump(const char*);
+void doTrace(const char*);
+void doInput(const char*);
+void doOutput(const char*);
+
 void doMenu() {
 	char input[512];
 	cout << mem.getRamSize() << "B RAM   " << mem.getRomSize() << "B ROM\n";
 	while(true) {
 		cout << "(l)oad  e(x)ecute  (d)ump  (t)race  (i)nput  (o)utput  (q)uit: ";
+		input[2]=0;
 		cin.getline(input,sizeof(input));
 		if(cin.eof())
 			break;
 		switch(toupper(input[0])) {
-			case 'L': {
-					clog << "TODO: LOAD\n";	// TODO
-				  } break;
-			case 'X': {
-					clog << "TODO: EXECUTE\n"; // TODO
-				  } break;
-			case 'D': {
-					clog << "TODO: DUMP\n"; // TODO
-				  } break;
-			case 'T': {
-					clog << "TODO: TRACE\n"; // TODO
-				  } break;
-			case 'I': {
-					clog << "TODO: INPUT\n"; // TODO
-				  } break;
-			case 'O': {
-					clog << "TODO: OUTPUT\n"; // TODO
-				  } break;
+			case 'L': doLoad(input+2); break;
+			case 'X': doExecute(); break;
+			case 'I': doInput(input+2); break;
+			case 'O': doOutput(input+2); break;
 			case 'Q': return;
 			default: cout << "Invalid command.\n";
 		}
 	}
+}
+
+void doLoad(const char* fName) {
+	string name = fName;
+	if(!name.length()) {
+		char buffer[512];
+		cout << "Enter filename (with or without .pepo): ";
+		cin.getline(buffer,sizeof(buffer));
+		name=buffer;
+	}
+	ifstream source;
+	source.open(name.c_str());
+	if(!source.good()) {
+		source.close();
+		source.open((name+".pepo").c_str());
+		if(!source.good()) {
+			cerr << "Error: Couldn't open specified file\n";
+			return;
+		}
+	}
+	cpu.setSP(mem.getUW(0xFFFA));
+	cpu.setPC(mem.getUW(0xFFFC));
+	while(cpu.doInstruction(source,clog));
+	source.close();
+}
+
+void doExecute() {
+	cpu.setSP(mem.getUW(0xFFF8));
+	cpu.setPC(0x0000);
+	while(cpu.doInstruction(settings.inputFile?*settings.inputFile:cin,settings.outputFile?*settings.outputFile:cout));
+}
+
+void doInput(const char* fName) {
+	string name = fName;
+	if(!name.length()) {
+		char buffer[512];
+		cout << "Enter filename (null for STDIN): ";
+		cin.getline(buffer,sizeof(buffer));
+		name=buffer;
+	}
+	delete settings.inputFile;
+	if(name.length())
+		settings.inputFile = new ifstream(name.c_str());
+	else
+		settings.inputFile = 0;
+}
+
+void doOutput(const char* fName) {
+	string name = fName;
+	if(!name.length()) {
+		char buffer[512];
+		cout << "Enter filename (null for STDOUT): ";
+		cin.getline(buffer,sizeof(buffer));
+		name=buffer;
+	}
+	delete settings.outputFile;
+	if(name.length())
+		settings.outputFile = new ofstream(name.c_str());
+	else
+		settings.outputFile = 0;
 }
